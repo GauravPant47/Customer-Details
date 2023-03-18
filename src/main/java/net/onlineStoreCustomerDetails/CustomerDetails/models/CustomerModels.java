bda2c1f32dd405b969a10cdb66fef19fb745c9a0
Gaurav <gaurav556pant@gmail.com>
2023-03-18 11:28:52 +0530

    Some Changes inside CustomerModels.java

--- src/main/java/net/onlineStoreCustomerDetails/CustomerDetails/models/CustomerModels.java
+++ src/main/java/net/onlineStoreCustomerDetails/CustomerDetails/models/CustomerModels.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "customer")
 public class CustomerModels {
@@ -16,84 +17,101 @@ public class CustomerModels {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "customer_id")
 	private int customerId;
-	
+
 	@Column(name = "customer_name")
 	private String customerName;
-	
+
 	@Column(name = "mobile_number")
 	private Long mobileNumber;
-	
+
 	@Column(name = "date_of_birth")
 	private Date dateOfBirth;
-	
+
 	@Column(name = "email_id")
 	private String emailId;
-	
+
 	@Column(name = "address")
 	private String address;
-	
+
 	@Column(name = "city")
 	private String city;
-	
-	@Column(name = "State")
+
+	@Column(name = "state")
 	private String State;
-	
+
 	@Column(name = "pincode")
 	private long pincode;
+
 	public int getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(int customerId) {
 		this.customerId = customerId;
 	}
+
 	public String getCustomerName() {
 		return customerName;
 	}
+
 	public void setCustomerName(String customerName) {
 		this.customerName = customerName;
 	}
+
 	public Long getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(Long mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
+
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
+
 	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getState() {
 		return State;
 	}
+
 	public void setState(String state) {
 		State = state;
 	}
+
 	public long getPincode() {
 		return pincode;
 	}
+
 	public void setPincode(long pincode) {
 		this.pincode = pincode;
 	}
-	
-	
+
 }
