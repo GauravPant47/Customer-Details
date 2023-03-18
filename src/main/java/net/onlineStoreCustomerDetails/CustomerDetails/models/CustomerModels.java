5a6ea593b61c9dbe11eebfdef2a741ca30772d9f
Gaurav <gaurav556pant@gmail.com>
2023-03-18 10:00:22 +0530

    create a models package and inside this create a CustomerModel.java and set customer regading Details

---
+++ src/main/java/net/onlineStoreCustomerDetails/CustomerDetails/models/CustomerModels.java
@@ -0,0 +1,71 @@
+package net.onlineStoreCustomerDetails.CustomerDetails.models;
+
+import java.util.Date;
+
+public class CustomerModels {
+	private int customerId;
+	private String customerName;
+	private Long mobileNumber;
+	private Date dateOfBirth;
+	private String emailId;
+	private String address;
+	private String city;
+	private String State;
+	private long pincode;
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public Long getMobileNumber() {
+		return mobileNumber;
+	}
+	public void setMobileNumber(Long mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public long getPincode() {
+		return pincode;
+	}
+	public void setPincode(long pincode) {
+		this.pincode = pincode;
+	}
+	
+	
+}
